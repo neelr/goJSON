9390d872a11a056c2f07fad887c45a0523e8a97b
Neel Redkar <neel.redkar@outlook.com>
2020-05-16 04:13:53 -0700

    title

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,13 @@ import Nav from "../components/Nav"
 import Sidebar from "../components/Sidebar"
 import Footer from "../components/Footer"
 import { Flex } from "rebass"
+import Head from "next/head"
 
 export default ({ Component, props }) => (
     <Flex height="100vh" flexDirection="column">
+        <Head>
+            <title>goJSON Dashboard</title>
+        </Head>
         <ThemeProvider theme={theme}>
             <Nav />
             <Flex height="100%">
