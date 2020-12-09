8188d2390f03688bddc4b2d995b321a0b582ac8e
Neel Redkar <hi@neelr.dev>
2020-12-09 03:00:23 +0000

    Switch from filesystem database to repl.it's redis like DB

--- db/db.go
+++ db/db.go
@@ -1,32 +1,30 @@
 package db
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
+	"github.com/replit/database-go"
 )
 
 // Write to the database dir
 func Write(id string, data string) {
-	ioutil.WriteFile(fmt.Sprintf(`database/%s.json`, id), []byte(data), 0644)
+	database.Set(id, data)
 }
 
 // Read the database dir
 func Read(id string) []byte {
-	content, err := ioutil.ReadFile(fmt.Sprintf(`database/%s.json`, id))
+	content, err := database.Get(id)
 	if err != nil {
 		return nil
 	}
-	return content
+	return []byte(content)
 }
 
 // Remove File
 func Remove(id string) {
-	os.Remove(fmt.Sprintf(`database/%s.json`, id))
+	database.Delete(id)
 }
 
 // Find a file
 func Find(id string) bool {
-	_, err := os.Stat(fmt.Sprintf(`database/%s.json`, id))
+	_, err := database.Get(id)
 	return err == nil
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/bitly/go-simplejson v0.5.0
+	github.com/replit/database-go v0.0.0-20200814154522-f5159eae301b
 	github.com/rs/cors v1.7.0
 )
